48aba470f719bd33fab70e01c8706e694992b7e5
Jor02 <Jor02games@gmail.com>
2021-10-08 15:55:28 +0200

    Removed unused testing php document

--- product/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('../classes/LaptopBase.php');
-
-    $pageID = number_format($_GET["id"]);
-
-    //echo var_dump(LaptopBase::GetLaptops(1, 3));
-    LaptopBase::GetLaptopList()
-?>
-
