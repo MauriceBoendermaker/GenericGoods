7966795901b8da1168c6cbb1c8e799bb61f7b28b
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2021-11-10 12:25:05 +0100

    Added Inches to Screen Diagonal

--- template.php
+++ template.php
@@ -41,73 +41,59 @@ while($row = $result->fetch_assoc()) {
 				<img class="productImage" width="640" height="360" src="./images/<?php echo $row["thumbnail"]; ?>">
 			</div>
 			<div class="spec-container">
-				<h3>Specificaties:</h3>
-				<table>
-					<tr>
-						<td>Merk:</td>
-						<td><?php echo $brandName["name"]; ?></td>
-					</tr>
-					<tr>
-						<td>Type:</td>
-						<td><?php echo $row["name"]; ?></td>
-					</tr>
-					<tr>
-						<td>Processor:</td>
-						<td><?php echo $cpuName["name"]; ?></td>
-					</tr>
-					<tr>
-						<td>Videokaart:</td>
-						<td><?php echo $gpuName["name"]; ?></td>
-					</tr>
-					<tr>
-						<td>Intern werkgeheugen (RAM):</td>
-						<td><?php echo $row["ram"]; ?></td>
-					</tr>
-					<tr>
-						<td>Schermdiagonaal:</td>
-						<td><?php echo $row["diagonal"]; ?></td>
-					</tr>
-					<tr>
-						<td>Resolutie:</td>
-						<td><?php echo $resolutionAmount["resolution"]; ?></td>
-					</tr>
-					<tr>
-						<td>Refreshrate:</td>
-						<td><?php echo $row["refreshrate"] . "Hz"; ?></td>
-					</tr>
-					<tr>
-						<td>Totale opslagcapaciteit:</td>
-						<td><?php echo $row["storagesize"]; ?></td>
-					</tr>
-					<tr>
-						<td>Type opslag:</td>
-						<td><?php echo $row["storagetype"]; ?></td>
-					</tr>
-				</table>
+				<div class="prijs">
+					<h3>Prijs: &euro;<?php echo $row["price"]; ?></h3>
+				</div>
+				<div class="spec-list">
+					<h3>Specificaties:</h3>
+					<table>
+						<tr>
+							<td>Merk:</td>
+							<td><?php echo $brandName["name"]; ?></td>
+						</tr>
+						<tr>
+							<td>Type:</td>
+							<td><?php echo $row["name"]; ?></td>
+						</tr>
+						<tr>
+							<td>Processor:</td>
+							<td><?php echo $cpuName["name"]; ?></td>
+						</tr>
+						<tr>
+							<td>Videokaart:</td>
+							<td><?php echo $gpuName["name"]; ?></td>
+						</tr>
+						<tr>
+							<td>Intern werkgeheugen (RAM):</td>
+							<td><?php echo $row["ram"]; ?></td>
+						</tr>
+						<tr>
+							<td>Schermdiagonaal:</td>
+							<td><?php echo $row["diagonal"] . "\""; ?></td>
+						</tr>
+						<tr>
+							<td>Resolutie:</td>
+							<td><?php echo $resolutionAmount["resolution"]; ?></td>
+						</tr>
+						<tr>
+							<td>Refreshrate:</td>
+							<td><?php echo $row["refreshrate"] . "Hz"; ?></td>
+						</tr>
+						<tr>
+							<td>Totale opslagcapaciteit:</td>
+							<td><?php echo $row["storagesize"]; ?></td>
+						</tr>
+						<tr>
+							<td>Type opslag:</td>
+							<td><?php echo $row["storagetype"]; ?></td>
+						</tr>
+					</table>
+				</div>
 			</div>
 			<div class="side-space">
 				<!-- side space -->
 			</div>
 		</div>
-
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-
-	<div style="background-color: lightgray; width: 250px;" class="column1">
-		<div class="<?php echo $brandName["name"]; ?>">
-			<div class='col-md-12'>
-				<img class='mx-auto' style='width: 100%' src="./images/<?php echo $row["thumbnail"]; ?>">
-			</div>
-			<div class='col-md-12 brandName'>
-				<a href="template.php?id=<?php echo $row["identifier"]; ?>"><?php echo $brandName["name"]." ".$row["name"]; ?></a>
-			</div>
-			<div class='col-md-12'><p><?php echo $gpuName["name"]; ?></p></div>
-			<div class='col-md-6'><p><?php echo $row["storagesize"]; ?></p></div>
-			<div class='col-md-6'><p><?php echo $row["storagetype"]; ?></p></div>
-		</div>
-	</div>
 
 <?php
 }
