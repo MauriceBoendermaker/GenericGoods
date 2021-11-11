d8e71764e5591bf0abd5ad387a3461f8126de4d2
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2021-11-11 16:27:19 +0100

    Made first 2 lines more readable

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php require "config.php"; ?>
-<?php require "include/head.php"; ?>
+<?php
+
+require "config.php";
+require "include/head.php";
+
+?>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <style>
